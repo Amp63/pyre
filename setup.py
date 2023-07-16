52538d6ec8ef92766e0c3986239517703a10edb3
Amp63 <milesburkart2299@gmail.com>
2023-07-16 19:38:05 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from setuptools import setup, find_packages
+
 setup(
     name='dfpyre',
-    version='0.3.0',
+    version='0.3.1',
     description='A package for externally creating code templates for the DiamondFire Minecraft server.',
     author='Amp',
     url='https://github.com/Amp63/pyre',
     license='MIT',
     keywords=['diamondfire', 'minecraft'],
     packages=find_packages(),
+    package_data={'dfpyre': ['data/data.json']}
 )
