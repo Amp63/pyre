bb983b6810676bef0c711c56b4dc4a0608b1d800
Amp63 <milesburkart2299@gmail.com>
2023-07-18 23:39:49 -0400

    update version number to 0.3.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dfpyre',
-    version='0.3.2',
+    version='0.3.3',
     description='A package for externally creating code templates for the DiamondFire Minecraft server.',
     author='Amp',
     url='https://github.com/Amp63/pyre',
