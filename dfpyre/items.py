82f745a1b68add300b9e6fb342a50305a74decfc
Amp63 <milesburkart2299@gmail.com>
2024-07-16 11:44:59 -0400

    fix parameter default value bug

--- dfpyre/items.py
+++ dfpyre/items.py
@@ -363,7 +363,10 @@ def item_from_dict(item_dict: dict) -> object:
         note = _some_or(item_data.get('note'), '')
         param_type = ParameterType(PARAMETER_TYPE_LOOKUP.index(item_data['type']))
         if item_data['optional']:
-            param = parameter(item_data['name'], param_type, item_data['plural'], True, description, note, item_from_dict(item_data['default_value']))
+            if 'default_value' in item_data:
+                param = parameter(item_data['name'], param_type, item_data['plural'], True, description, note, item_from_dict(item_data['default_value']))
+            else:
+                param = parameter(item_data['name'], param_type, item_data['plural'], True, description, note)
         else:
             param = parameter(item_data['name'], param_type, item_data['plural'], False, description, note)
         return param
